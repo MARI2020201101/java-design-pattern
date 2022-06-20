7ce2296904d0850aa239c1e819b4765f47d8720f
MARI <mari20201101@gmail.com>
2022-06-20 17:58:14 +0900

    Practice static decorator pattern

--- src/decorator/Shape.java
+++ src/decorator/Shape.java
@@ -1,5 +1,7 @@
 package decorator;
 
+import java.util.function.Supplier;
+
 interface Shape {
     String info();
 }
@@ -60,8 +62,41 @@ class TransparentShape implements Shape{
         return shape.info() + " with transparency percent " + transparency + " %";
     }
 }
+
+class ColoredShapeV2<T extends Shape> implements Shape{
+    private Shape shape;
+    private String color;
+
+    public ColoredShapeV2(Supplier<? extends T> constructor, String color) {
+        this.shape = constructor.get();
+        this.color = color;
+    }
+    @Override
+    public String info() {
+        return shape.info() + " with color "+ color;
+    }
+}
+
+class TransparentShapeV2<T extends Shape> implements Shape{
+    private Shape shape;
+    private int transparency;
+
+    public TransparentShapeV2(Supplier<? extends T> constructor, int transparency) {
+        this.shape = constructor.get();
+        this.transparency = transparency;
+    }
+    @Override
+    public String info() {
+        return shape.info() + " with transparency percent " + transparency + " %";
+    }
+}
+
 class Demo2{
     public static void main(String[] args) {
+
+        //dynamic decorator
+        //항상 새로운 데코레이터를 만든다.
+        //컴파일 타임 vs 런타임
         Circle circle = new Circle(10);
         System.out.println(circle.info());
         ColoredShape coloredShape = new ColoredShape(circle, "blue");
@@ -71,5 +106,17 @@ class Demo2{
         System.out.println(transparentShape.info());
         ColoredShape withTs = new ColoredShape(transparentShape, "green");
         System.out.println(withTs.info());
+
+        System.out.println("=========================");
+        //static decorator composition
+        ColoredShapeV2<Circle> coloredShapeV2 = new ColoredShapeV2<>(
+                ()->new Circle(100), "pink");
+        System.out.println(coloredShapeV2.info());
+
+        TransparentShapeV2<ColoredShapeV2<Square>> transparentShapeV2 = new TransparentShapeV2<>(
+                ()-> new ColoredShapeV2<>(
+                        ()-> new Square(1),"white")
+                , 50);
+        System.out.println(transparentShapeV2.info());
     }
 }
